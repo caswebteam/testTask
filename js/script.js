b68ae165ba91e6665ae54591624ce5556bb51d19
caswebteam <studioartcanvas@gmail.com>
2017-02-20 17:39:43 +0200

    трек

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function() {
+	
+	$.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ('Authorization', '337e2f6e0dcf7da587ad93e5a452fb3569314889');
+        },
+        headers: {
+            'Authorization': '337e2f6e0dcf7da587ad93e5a452fb3569314889'
+        },
+        type: 'get',
+        url: "http://504080.com/api/v1/services/categories",
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(results){
+            console.log(results);
+        },
+
+    })
+
+})();
